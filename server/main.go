6c0160d74a3c2d742647dfab3e9b6978869b7ef2
Matthew Tapps <mail@matthewtapps.com>
2025-01-13 10:54:52 +1100

    Create CLI to send messages to agent via websocket

---
+++ server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return errors.New("must specify the address that the agent is listening on as the first argument")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	connection, _, err := websocket.Dial(ctx, os.Args[1], &websocket.DialOptions{
+		Subprotocols: []string{"echo"},
+	})
+	if err != nil {
+		return err
+	}
+	defer connection.Close(websocket.StatusInternalError, "connection to agent closed")
+
+	fmt.Println("Enter message for agent and submit")
+
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			fmt.Print("-> ")
+			err = wsjson.Write(ctx, connection, map[string]string{
+				"type":    "echo",
+				"message": scanner.Text(),
+			})
+			if err != nil {
+				return err
+			}
+			v := map[string]string{}
+			err = wsjson.Read(ctx, connection, &v)
+			if err != nil {
+				return err
+			}
+			fmt.Println(v)
+		}
+	}
+}
